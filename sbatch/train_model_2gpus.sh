f0a8c7bfb153eed5670e52de6d0e78cf59df8fde
Thomas Rieutord <thomas.rieutord@met.ie>
2024-08-08 11:09:50 +0000

    Experiment training on 2 GPUs

---
+++ sbatch/train_model_2gpus.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# The job name
+#SBATCH --job-name=trainlam
+# Set the error and output files
+#SBATCH --output=trainlam-%J.out
+#SBATCH --error=trainlam-%J.out
+# Set the initial working directory
+#SBATCH --chdir=/scratch/dutr/spool
+# Choose the queue
+#SBATCH --qos=ng
+#SBATCH --gpus=2
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem=128GB
+# Wall clock time limit
+#SBATCH --time=4:00:00
+# Send an email on failure
+#SBATCH --mail-type=FAIL
+# This is the job
+date
+echo "Running on $HOSTNAME:$PWD"
+
+module load conda
+mamba activate neurallam
+
+echo "Env successfully loaded!"
+python --version
+date
+
+DATASET=mera_4years_fullres
+BATCHSIZE=4
+EPOCHS=10
+N_WORKERS=16
+
+set -vx
+
+python $HOME/neural-lam/scripts/train_model.py --dataset $DATASET --batch_size $BATCHSIZE --ar_steps 2 --step_length 1 --control_only 1 --epochs $EPOCHS --n_workers $N_WORKERS --gpus 2
+
+date
